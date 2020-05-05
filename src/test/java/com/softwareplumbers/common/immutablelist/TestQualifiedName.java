f1f7ad4d03eca1adf5d88eeb09bcd67850bf826e
Jonathan Essex <jonathan.essex@softwareplumbers.com>
2020-05-05 11:01:29 -0400

    Explanatory comments

--- src/test/java/com/softwareplumbers/common/immutablelist/TestQualifiedName.java
+++ src/test/java/com/softwareplumbers/common/immutablelist/TestQualifiedName.java
@@ -21,10 +21,18 @@ public class TestQualifiedName {
 
 	@Test
 	public void testApply() {
+        // We are using maps instead of JsonObjects here because we don't want to create
+        // a depenency on a json implementation.
+        
+        // m1 is equivalent to a Json object: { "a" : "A", "b" : "B" }
 		Map<String,Object> m1 = new TreeMap<>(); m1.put("a", "A"); m1.put("b", "B");
+        // m2 is equivalent to a Json object: { "c" : "C", "d" : "D" }
 		Map<String,Object> m2 = new TreeMap<>(); m2.put("c", "C"); m2.put("d", "D");
+        // m3 is equivalent to a Json object: { "x" : { "a" : "A", "b" : "B" }, y : { "c" : "C", "d" : "D" } }
 		Map<String,Object> m3 = new TreeMap<>(); m3.put("x",m1); m3.put("y", m2);
+        // QualifiedName.of("x","b") is x.b
 		assertEquals("B", QualifiedName.of("x","b").apply(m3));
+        // QualifiedName.of("x","b") is y.c
 		assertEquals("C", QualifiedName.of("y","c").apply(m3));
 		assertNull(QualifiedName.of("y","a").apply(m3));
 		assertNull(QualifiedName.of("y","a","x").apply(m3));
